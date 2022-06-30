60fcea1e60cfb297f37bb80712a2fe30388a5057
passiondev0503 <passiondev0503@gmail.com>
2022-06-30 14:45:11 +0700

    integation with redis save messages

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,4 +1,7 @@
-import { Logger } from '@nestjs/common';
+//! Please Install >> redis-server <<
+//TODO link tutorial from Redis => https://www.learmoreseekmore.com/2020/12/nestjs-redis-cache.html, Socket.io => https://gabrieltanner.org/blog/nestjs-realtime-chat/
+
+import { CACHE_MANAGER, Inject, Logger } from '@nestjs/common';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
@@ -7,6 +10,7 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { Cache } from 'cache-manager';
 import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({
@@ -17,12 +21,41 @@ import { Socket, Server } from 'socket.io';
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('AppGateway');
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, payload: string): void {
-    console.log(payload);
+  async handleMessage(client: Socket, payload: any): Promise<void> {
+    const messaage: any = payload;
+    const valueMessage: any = await this.cacheManager.get('messages');
+    let updateMessage: any[] = [];
+
+    if (valueMessage === null) {
+      updateMessage = [
+        {
+          sender: messaage.name,
+          message: messaage.text,
+          dateTime: Date.now(),
+        },
+      ];
+    } else {
+      const recodeString = JSON.parse(valueMessage);
+      updateMessage = [
+        ...recodeString,
+        {
+          sender: messaage.name,
+          message: messaage.text,
+          dateTime: Date.now(),
+        },
+      ];
+    }
+    await this.cacheManager.set('messages', JSON.stringify(updateMessage), {
+      ttl: 300,
+    });
+
+    console.log(JSON.parse(valueMessage));
     this.server.emit('msgToClient', payload);
   }
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app.gateway';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, AppGateway],
 })
